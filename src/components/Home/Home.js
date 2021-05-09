1393272efe57a30b080d4598df40e324cddef1c2
bhagat-hrishi <hrishibhagat8642@gmail.com>
2021-05-09 14:51:35 +0530

    Adding features component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Nav from '../Nav/Nav'
 
 import SloganMaker from '../SloganMaker/SloganMaker'
+import Features from '../Features/Features'
 import './Home.css'
 const Home=()=>{
     return (
        <div className='home'>
         <Nav />
         <SloganMaker/>
+        <Features/>
        </div>
     )
 }
